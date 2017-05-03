29dcb25e18093ec8697cc98b254324e1ce71c803
Max <max.trewhitt@student.kyh.se>
2017-05-03 22:49:08 +0200

    sidebar now opens, arrow animations implemented

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,19 +6,34 @@ import ReferenceList from './ReferenceList'
 
 import styles from './sidebar.css'
 
-
 class Sidebar extends React.Component {
 
     constructor() {
         super()
+        this.onclick = this.onclick.bind(this);
     }
 
     onclick() {
+    	let arrow = this.refs.button.children[0]
         let sidebar = this.refs.button.parentElement
-        sidebar.style.marginLeft = '-316px'
-
         let mainPane = sidebar.nextElementSibling
-        mainPane.style.width = "calc(100% - 34px)"
+
+        if (!sidebar.classList.contains('collapse')) {
+        	arrow.style.animation = 'spinRight 1s 1 alternate'
+        	arrow.style.animationFillMode = 'forwards'
+
+        	sidebar.classList.add('collapse')
+        	sidebar.style.marginLeft = '-316px'
+
+        	mainPane.style.width = 'calc(100% - 34px)'
+        } else {
+        	arrow.style.animation = 'spinLeft 1s 1 alternate'
+
+        	sidebar.classList.remove('collapse')
+        	sidebar.style.marginLeft = '0'
+
+        	mainPane.style.width = 'calc(100% - 350px)'
+        }
     }
 
     render() {
@@ -33,7 +48,7 @@ class Sidebar extends React.Component {
                     <h2>Referenslänkar</h2>
                     <ReferenceList user={this.props.user} />
                 </div>
-                <a className={styles.hamburger} href="#" onClick={this.onclick.bind(this)} ref="button"><FA name='angle-double-left' /></a>
+                <a className={styles.hamburger} href="#" onClick={this.onclick} ref="button"><FA name='angle-double-left' /></a>
             </div>
         );
     }
